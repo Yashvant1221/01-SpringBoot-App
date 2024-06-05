a4e60351dea59397301a42663c22127bc4ea0e2b
Yashvant123 <yashvantkumar0032@gmail.com>
2024-06-05 11:10:32 +0530

    HIS-300: Realted Changes

--- src/main/java/in/ashokait/Application.java
+++ src/main/java/in/ashokait/Application.java
@@ -7,6 +7,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class Application {
 
+       private static final String REST_URL = "https://www.equifax.com/getScores";
+
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
 		//Changes for HIS-134
